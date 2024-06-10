c8e0f98240be6e1d23a645171be33e47524179a1
amine <amine.tamouh.officiel@gmail.com>
2024-06-10 12:21:44 +0100

    create micro-services for all modules

---
+++ Address/Address/src/test/java/org/idb/Address/AddressApplicationTests.java
@@ -0,0 +1,13 @@
+package org.idb.Address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
