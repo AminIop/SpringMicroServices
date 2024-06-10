f44bff3059365411b3320377be54d91f055f21ea
amine <amine.tamouh.officiel@gmail.com>
2024-06-10 15:43:15 +0100

    add user entity

---
+++ User/User/src/main/java/org/idb/User/entity/User.java
@@ -0,0 +1,24 @@
+package org.idb.User.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "\"user\"")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private int address_id;
+}
