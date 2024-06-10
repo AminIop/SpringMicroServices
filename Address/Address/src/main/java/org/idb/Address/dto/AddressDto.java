e53d9c45a5f5459c947a92718ab5d822b637fa3b
amine <amine.tamouh.officiel@gmail.com>
2024-06-10 12:28:50 +0100

    create Address dto pattern

---
+++ Address/Address/src/main/java/org/idb/Address/dto/AddressDto.java
@@ -0,0 +1,16 @@
+package org.idb.Address.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class AddressDto {
+    private String street;
+    private String city;
+    private String country;
+}
